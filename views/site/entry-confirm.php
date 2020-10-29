fbc27a5c2d8198a96ca2e14f6975845ae7e203df
Paradise <shinodise@gmail.com>
2020-10-29 09:00:09 +0300

    +smart

--- views/site/entry-confirm.php
+++
@@ -1,6 +0,0 @@
-<?php
-use yii\helpers\Html;
-?>
-<p>Вы ввели следующую информацию:</p>
-<li><label>Email</label>: <?= Html::encode($model->email) ?></li>
-<li><label>Имя</label>: <?= Html::encode($model->name) ?></li>
